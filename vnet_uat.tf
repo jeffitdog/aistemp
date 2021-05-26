df7db1292e7a4551639b09baa33fee42f2ad402e
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-26 20:34:20 +0800

    test3

--- vnet_uat.tf
+++ vnet_uat.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg_uat" {
   name     = "RG-UAT-10"
-  location = "West Europe"     #Change the location to chinaeast2
+  location = "southeastasia"     #Change the location to chinaeast2
   
   tags     = {
     environment = "dev"  #Update the tag
