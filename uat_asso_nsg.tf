b71a44f41b6c2637d27132bc981ba2216d94dbc3
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-27 19:44:34 +0800

    new

--- uat_asso_nsg.tf
+++ uat_asso_nsg.tf
@@ -1,7 +1,11 @@
+/*
 module "NSG_UAT_internal" {
   source                        = "./module/terraform-azure-nsg-association"
-  network_security_group_name   = module.NSG_UAT_internal.NSG-UAT-internal
+  network_security_group_name   = "module.NSG_UAT_internal.NSG-UAT-internal"
   subnet_name                   = module.vnet_uat.subnet_names[1]
   virtual_network_name          = module.vnet_uat.vnet_name
   resource_group_name           = azurerm_resource_group.rg_uat.name
 }
+
+
+*/
