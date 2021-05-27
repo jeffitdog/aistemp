62d4d3a906af4cf73019bc90e131b72964194612
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-27 20:20:12 +0800

    asso

--- uat_asso_nsg.tf
+++ uat_asso_nsg.tf
@@ -1,4 +1,12 @@
-module "NSG_UAT_internal" {
+module "asso_dmz" {
+  source                        = "./module/terraform-azure-nsg-association"
+  network_security_group_name   = var.nsg_name[0]
+  subnet_name                   = var.subnet_name[0]
+  virtual_network_name          = var.vnet_name
+  resource_group_name           = var.rg_name
+}
+
+module "asso_int" {
   source                        = "./module/terraform-azure-nsg-association"
   network_security_group_name   = var.nsg_name[1]
   subnet_name                   = var.subnet_name[1]
