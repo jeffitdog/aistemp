027d6496ac4d3d6f66930875a6edc73592a3fa63
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-27 20:40:25 +0800

    day one

--- uat_asso_nsg.tf
+++ uat_asso_nsg.tf
@@ -5,7 +5,7 @@ module "asso_dmz" {
   virtual_network_name          = var.vnet_name
   resource_group_name           = var.rg_name
 
-  depends_on = [module.NSG_UAT_DMZ]
+  depends_on = [module.vnet_uat]
 }
 
 module "asso_int" {
@@ -15,5 +15,5 @@ module "asso_int" {
   virtual_network_name          = var.vnet_name
   resource_group_name           = var.rg_name
 
-  depends_on = [module.NSG_UAT_internal]
+  depends_on = [module.vnet_uat]
 }
