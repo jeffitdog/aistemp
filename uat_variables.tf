a6af21d4129828af5490e6e0a5ca8ebcebe403fd
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-27 20:04:41 +0800

    var

--- uat_variables.tf
+++ uat_variables.tf
@@ -10,5 +10,10 @@ variable "vnet_name" {
 
 variable "subnet_name" {
   type    = any
-  default = []
+  default = ["SNT-UAT-DMZ-64", "SNT-UAT-internal-65"]
+}
+
+variable "nsg_name" {
+  type    = any
+  default = ["NSG-UAT-DMZ", "NSG-UAT-internal"]
 }
--- uat_vnet.tf
+++ uat_vnet.tf
@@ -3,7 +3,7 @@ module "vnet_uat" {
   resource_group_name = azurerm_resource_group.rg_uat.name
   address_spaces      = ["10.10.64.0/20"]
   subnet_prefixes     = ["10.10.64.0/24", "10.10.65.0/24"]
-  subnet_names        = ["SNT-UAT-DMZ-64", "SNT-UAT-internal-65"]
+  subnet_names        = [var.subnet_name[0], var.subnet_name[1]]
   vnet_name           = var.vnet_name
   tags = {
     ENV = "Prod"
