2ac114947e181cd4b6f90ee06201cd375482d5de
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-28 10:35:17 +0800

    another start

--- uat_nsg.tf
+++ uat_nsg.tf
@@ -326,3 +326,22 @@ module "NSG_UAT_internal" {
   depends_on = [azurerm_resource_group.rg_uat]
 }
 
+module "asso_dmz" {                       #associate nsg to the subnet
+  source                        = "./module/terraform-azure-nsg-association"
+  network_security_group_name   = var.nsg_name[0]
+  subnet_name                   = var.subnet_name[0]
+  virtual_network_name          = var.vnet_name
+  resource_group_name           = var.rg_name
+
+  depends_on = [module.NSG_UAT_DMZ]
+}
+
+module "asso_int" {                         #associate nsg to the subnet
+  source                        = "./module/terraform-azure-nsg-association"
+  network_security_group_name   = var.nsg_name[1]
+  subnet_name                   = var.subnet_name[1]
+  virtual_network_name          = var.vnet_name
+  resource_group_name           = var.rg_name
+
+  depends_on = [module.NSG_UAT_internal]
+}
