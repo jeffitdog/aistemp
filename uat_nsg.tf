a822c242be1f528acbe28180c9bfc2cbd510ace9
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-30 00:15:42 +0800

    test

--- uat_nsg.tf
+++ uat_nsg.tf
@@ -1,13 +1,4 @@
 #Must create resource group first 
-resource "azurerm_resource_group" "rg_uat" {
-  name     = var.rg_name
-  location = "southeastasia"     #Change the location to chinaeast2
-  
-  tags     = {
-    environment = "dev"  #Update the tag
-  }
-}
-
 
 #Create NSG Rules
 module "NSG_UAT_DMZ" {
