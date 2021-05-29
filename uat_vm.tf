e08d8e40c87d251cfd7c1a09b91104a1776d6e82
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-29 23:58:33 +0800

    VM

--- uat_vm.tf
+++ uat_vm.tf
@@ -10,11 +10,12 @@ module "linuxservers" {
  vm_hostname = ["public-vm-1","public-vm-2","private-vm-1"]
   #vm_hostname = "my-vm"
   nb_instances    = 1
-  nb_public_ip    = 1
+  nb_public_ip    = 0
   vm_os_offer     = "CentOS"
   vm_os_publisher = "OpenLogic"
   vm_os_sku       = "7_9"
   vm_os_version   = "latest"
-  vm_size         = "Standard_F2s_v2"
-  source_address_prefixes   = ["13.67.111.51"]
+  vm_size         = "Standard_F4s_v2"
+  data_sa_type    = "Standard_LRS"
+  #source_address_prefixes   = ["13.67.111.51"]
 }
