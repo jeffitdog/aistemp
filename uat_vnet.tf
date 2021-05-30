aedc61e8392aba76624c9029c2c69b12a798da78
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-30 14:10:16 +0800

    vm_size

--- uat_vnet.tf
+++ uat_vnet.tf
@@ -13,15 +13,14 @@ module "vnet_uat" {
   depends_on = [azurerm_resource_group.rg_uat]
 }
 
-/*"azurerm_virtual_network_peering" "uat_transit" {
+resource "azurerm_virtual_network_peering" "uat_transit" {
   name                      = "uattotransit"
   resource_group_name       = azurerm_resource_group.rg_uat.name
   virtual_network_name      = module.vnet_uat.vnet_name
-  remote_virtual_network_id = azurerm_virtual_network.example-2.id
+  remote_virtual_network_id = module.vnet_transit.vnet_id
 }
 
-*/
-
+*
 
 /*  
 resource "azurerm_network_interface" "vm" {
