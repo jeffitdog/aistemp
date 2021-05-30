8d5646983069f57fe64c19edb642c076e038f07e
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-30 15:06:53 +0800

    vnet

--- uat_vnet.tf
+++ uat_vnet.tf
@@ -1,7 +1,7 @@
 module "vnet_uat" {
   source              = "./module/terraform-azurerm-network"
   resource_group_name = azurerm_resource_group.rg_uat.name
-  address_spaces      = [var.vnet_space]
+  address_spaces      = var.vnet_space
   subnet_prefixes     = [var.subnet_space[0], var.subnet_space[1]]
   subnet_names        = [var.subnet_name[0], var.subnet_name[1]]
   vnet_name           = var.vnet_name
@@ -16,7 +16,7 @@ module "vnet_uat" {
 module "vnet_transit" {
   source              = "./module/terraform-azurerm-network"
   resource_group_name = azurerm_resource_group.rg_uat.name
-  address_spaces      = [var.transit_vnet_space]
+  address_spaces      = var.transit_vnet_space
   subnet_prefixes     = [var.transit_subnet_space[0], var.transit_subnet_space[1]]
   subnet_names        = [var.transit_subnet_name[0], var.transit_subnet_name[1]]
   vnet_name           = var.transit_vnet_name
