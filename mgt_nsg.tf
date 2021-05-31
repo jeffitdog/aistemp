779dba508cb12175043877dc0b98630b1d04cc88
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-31 15:31:42 +0800

    nsg

--- mgt_nsg.tf
+++ mgt_nsg.tf
@@ -13,7 +13,7 @@ module "NSG_MGT_Security" {
       protocol               = "tcp"
       source_port_range      = "*"
       destination_port_range = "3389"
-      source_address_prefix  = ["10.10.17.201,10.10.17.202,10.10.17.203,10.10.17.204,10.10.17.205,10.10.17.206,10.10.17.207,10.10.17.208"]
+      source_address_prefixes  = ["10.10.17.201,10.10.17.202,10.10.17.203,10.10.17.204,10.10.17.205,10.10.17.206,10.10.17.207,10.10.17.208"]
       destination_address_prefix = "10.10.48.0/24"
       description            = "堡垒机"
     },
