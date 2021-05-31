11142ece8233e8d577e24185b68c122405a6ca36
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-05-31 13:06:25 +0800

    vnet

--- vnet_peering.tf
+++ vnet_peering.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network_peering" "uat_mgt" {
   name                      = "UAT-To-Management"
   resource_group_name       = azurerm_resource_group.rg_uat.name
   virtual_network_name      = module.vnet_uat.vnet_name
-  remote_virtual_network_id = module.vnet_transit.vnet_id
+  remote_virtual_network_id = module.vnet_mgt.vnet_id
 }
 
 resource "azurerm_virtual_network_peering" "mgt_uat" {
