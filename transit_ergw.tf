9de682fa93c48a3d8b8c15dece59b8b876232102
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-06-04 14:54:15 +0800

    dfd

--- transit_ergw.tf
+++ transit_ergw.tf
@@ -1,31 +1,10 @@
-/*
-resource "azurerm_resource_group" "example" {
+
+resource "azurerm_resource_group" "erg_rg" {                   #建立屬於ExpressRoute的資源組
   name     = var.erg_rg
   location = var.region
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "test"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["192.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["192.0.1.0/27"]
-}
-*/
-
-
-resource "azurerm_resource_group" "erg_rg" {
-  name     = var.erg_rg
-  location = var.region
-}
-
-resource "azurerm_public_ip" "erg_pip" {
+resource "azurerm_public_ip" "erg_pip" {                            #建立Public IP address
   name                = "ERGW_GIP"
   location            = azurerm_resource_group.erg_rg.location
   resource_group_name = azurerm_resource_group.erg_rg.name
@@ -33,7 +12,7 @@ resource "azurerm_public_ip" "erg_pip" {
   allocation_method = "Dynamic"
 }
 
-resource "azurerm_virtual_network_gateway" "example" {
+resource "azurerm_virtual_network_gateway" "VPNGateway" {
   name                = "ERGW"
   location            = var.region 
   resource_group_name = var.transit_rg_name
@@ -54,3 +33,37 @@ resource "azurerm_virtual_network_gateway" "example" {
 }
 
 
+
+resource "azurerm_express_route_circuit" "example" {
+  name                  = "expressRoute1"
+  resource_group_name   = azurerm_resource_group.erg_rg.name
+  location              = azurerm_resource_group.erg_rg.location
+  service_provider_name = "Equinix"
+  peering_location      = "Silicon Valley"
+  bandwidth_in_mbps     = 50
+
+  sku {
+    tier   = "Standard"
+    family = "UnlimitedData"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+
+/*
+resource "azurerm_virtual_network_gateway_connection" "expressroutecircuit" {
+  name                = "expressroutecircuit"
+  location            = azurerm_resource_group.erg_rg.location
+  resource_group_name = azurerm_resource_group.erg_rg.name
+
+  type                       = "ExpressRoute"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.VPNGateway.id
+  express_route_circuit_id   = azurerm_local_network_gateway.onpremise.id
+
+  #shared_key = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
+}
+
+*/
